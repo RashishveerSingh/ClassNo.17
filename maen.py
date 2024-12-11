533f996d09c552f3983e488a37f32b0be366100b
RashishveerSingh <ashstormsaber@gmail.com>
2024-12-11 20:47:04 +0800

    enecial

---
+++ maen.py
@@ -0,0 +1,6 @@
+from datetime import date, time, datetime
+import calendar
+date_for_todey = date.today()
+current_datetaim = datetime.now()
+print(date_for_todey, current_datetaim)
+print(calendar.month(2001,9))
